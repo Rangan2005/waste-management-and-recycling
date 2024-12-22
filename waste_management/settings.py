9b7fb42f05b8bcbfa815b92bea8b0bb644080fa7
Subhrajyoti Basu <168283852+Rangan2005@users.noreply.github.com>
2024-12-22 22:27:30 +0530

    Update settings.py

--- waste_management/settings.py
+++ waste_management/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-b@0nw-4w(g1#b91xu75zm@_&546@-rny9m9c7^^bd%@whq7mno
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['waste-management-and-recycling.onrender.com']
 
 
 # Application definition
